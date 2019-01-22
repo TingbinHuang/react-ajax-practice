c7c7b0111190e4673bc5ed90723ea13426955596
Tingbin Huang <tingbinhuang@Tingbins-MacBook-Pro.local>
2019-01-21 18:53:04 -0800

    Fix indentation in App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -13,13 +13,11 @@ class App extends React.Component {
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
-
   handleChange(event) {
     this.setState({
       [event.target.name]: event.target.value
     });
   }
-
   handleSubmit(event) {
     event.preventDefault();
     var message = {
@@ -27,7 +25,6 @@ class App extends React.Component {
       message: this.state.message
     };
     $.ajax({
-
       url: 'http://ec2-13-57-25-101.us-west-1.compute.amazonaws.com:3000/api/hrsf111/greeting',
       type: 'POST',
       data: JSON.stringify(message),
@@ -43,10 +40,8 @@ class App extends React.Component {
       }
     });    
   }
-  
   render() {
     return (
-
       <div>
       <form onSubmit={this.handleSubmit}>
         <p>Server Response:{this.state.successMeg}</p>
@@ -54,18 +49,14 @@ class App extends React.Component {
           Name:
           <input type="text" name='name' onChange={this.handleChange} />
         </label>
-        
          <label> 
            Message:
            <input type='text' name='message' onChange={this.handleChange} />
         </label>
-        
         <input type="submit" value="Send Message"  />
       </form>
-
       </div>
     );
   }
 }
-
 export default App;
